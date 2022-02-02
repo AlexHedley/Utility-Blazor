7655402906a1f8e05f409bda7e9620b4196a89cc
Alex Hedley <alex.hedley@me.com>
2022-02-02 11:47:39 +0000

    #2 WIP

---
+++ src/Utility-Blazor.Test/MainLayoutTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+using Bunit;
+using Xunit;
+
+using Utility_Blazor.Shared;
+
+namespace Utility_Blazor.Test
+{
+    public class MainLayoutTest : TestContext
+    {
+        [Fact]
+        public async Task SavesNameToLocalStorage()
+        {
+            // Arrange
+            const string inputName = "John Smith";
+            var localStorage = this.AddBlazoredLocalStorage();
+            var cut = RenderComponent<MainLayout>();
+
+            // Act
+            cut.Find("#Name").Change(inputName);
+            cut.Find("#NameButton").Click();
+
+            // Assert
+            var name = await localStorage.GetItemAsync<string>("name");
+
+            Assert.Equal(inputName, name);
+        }
+    }
+}
--- src/Utility-Blazor/Program.cs
+++ src/Utility-Blazor/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using Blazored.LocalStorage;
+
 namespace Utility_Blazor
 {
     public class Program
@@ -18,6 +20,7 @@ namespace Utility_Blazor
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
