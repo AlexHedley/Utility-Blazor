cd0cac37ee86839e34e04ddf19b6e6c68421ba21
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-06-02 16:04:19 +0100

    #2 StringConverter

---
+++ src/Utility.Test/StringConverterTest.cs
@@ -0,0 +1,19 @@
+using Bunit;
+
+namespace Utility.Test;
+
+public class StringConverterTest: TestContext
+{
+    public StringConverterTest()
+    {
+    }
+
+    //[Fact]
+    //public void FieldShouldClearWhenClicked()
+    //{
+    //    var cut = RenderComponent<SQLBuilder>();
+    //    cut.Find("btnFieldClear").Click();
+
+    //    cut.Find("field").MarkupMatches("");
+    //}
+}
