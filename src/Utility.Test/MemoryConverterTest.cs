014b90824807eabd374409b8547db9ad0f1d097b
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-06-06 15:12:27 +0100

    #3 Updated Memory Converter Tests

--- src/Utility.Test/MemoryConverterTest.cs
+++ src/Utility.Test/MemoryConverterTest.cs
@@ -15,10 +15,13 @@ public class MemoryConverterTest: TestContext
     public void MemoryShouldConvertWhenClicked()
     {
         var cut = RenderComponent<MemoryConverter>();
-        cut.FindAll("button")[1].Click();
-
-        string markup = "<input id=\"output\" class=\"form-control\" value=\"1.0 KB\">";
         
+        cut.FindAll("button")[1].Click();
+        string markup = "<input id=\"output\" class=\"form-control\" placeholder=\"KB / MB / GB / TB / PB / EB / ZB / YB\" readonly=\"readonly\" value=\"1.0 KB\">";
         cut.FindAll("input")[1].MarkupMatches(markup);
+
+        MemoryConverter memoryConverter = cut.Instance;
+        Assert.Equal(memoryConverter.Input, 1030);
+        Assert.Equal(memoryConverter.Output, "1.0 KB");
     }
 }
