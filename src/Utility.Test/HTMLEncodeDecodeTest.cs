9c4f060e38ee04c9b1f8ab6ec964b2bd34da4903
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-06-08 15:42:25 +0100

    #3 HTML Encode / Decode

---
+++ src/Utility.Test/HTMLEncodeDecodeTest.cs
@@ -0,0 +1,48 @@
+using Bunit;
+using Xunit;
+
+using Utility.Components.HTMLEncodeDecode;
+
+namespace Utility.Test;
+
+public class HTMLEncodeDecodeTest : TestContext
+{
+    public HTMLEncodeDecodeTest()
+    {}
+
+    [Fact]
+    public void CanEncode()
+    {
+        var decoded = "&lt;html&gt;&lt;body&gt;&lt;h1&gt;Hello&lt;/h1&gt;&lt;/body&gt;&lt;/html&gt;";
+        var encoded = string.Empty;
+        
+        var cut = RenderComponent<HTMLEncodeDecode>(parameters => parameters
+            .Add(p => p.Decoded, decoded)
+            .Add(p => p.Encoded, encoded)
+        );
+        
+        cut.FindAll("button")[0].Click();
+
+        var converted = "<html><body><h1>Hello</h1></body></html>";
+        HTMLEncodeDecode htmlEncodeDecode = cut.Instance;
+        Assert.Equal(htmlEncodeDecode.Encoded, converted);
+    }
+    
+    // [Fact]
+    public void CanDecode()
+    {
+        var decoded = string.Empty;
+        var encoded = "<html><body><h1>Hello</h1></body></html>";
+        
+        var cut = RenderComponent<HTMLEncodeDecode>(parameters => parameters
+            .Add(p => p.Decoded, decoded)
+            .Add(p => p.Encoded, encoded)
+        );
+        
+        cut.FindAll("button")[2].Click();
+
+        var converted = "&lt;html&gt;&lt;body&gt;&lt;h1&gt;Hello&lt;/h1&gt;&lt;/body&gt;&lt;/html&gt;";
+        HTMLEncodeDecode htmlEncodeDecode = cut.Instance;
+        Assert.Equal(htmlEncodeDecode.Decoded, converted);
+    }
+}
