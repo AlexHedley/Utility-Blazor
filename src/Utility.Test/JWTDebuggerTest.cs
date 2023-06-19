d3b2d13ddcb4d2af39d2e0e2e004b0b6d8705a53
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-06-19 16:33:27 +0100

    #24 JWT

--- src/Utility.Test/JWTDebuggerTest.cs
+++ src/Utility.Test/JWTDebuggerTest.cs
@@ -9,25 +9,27 @@ public class JWTDebuggerTest : TestContext
 {
     public JWTDebuggerTest() {}
 
-    // [Fact]
-    // public void StringShouldDecode()
-    // {
-    //     string input = "QWxleEhlZGxleQ==";
-    //     string output = string.Empty;
-    //     string expectedOutput = "AlexHedley";
-    //     
-    //     var cut = RenderComponent<JWTDebugger>(parameters => parameters
-    //         .Add(p => p.Input, input)
-    //         .Add(p => p.Output, output)
-    //     );
-    //     
-    //     cut.FindAll("button")[0].Click();
-    //     // string markup = "";
-    //     // cut.FindAll("input")[1].MarkupMatches(markup);
-    //
-    //     JWTDebugger jwtDebugger = cut.Instance;
-    //     Assert.Equal(base64.Input, input);
-    //     Assert.Equal(base64.Output, expectedOutput);
-    // }
+    [Fact]
+    public void JwtTokenShouldDecode()
+    {
+        string input = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        string expectedHeader = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+        string expectedPayload = "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}";
+        string exptectedSignature = "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        
+        var cut = RenderComponent<JWTDebugger>(parameters => parameters
+            .Add(p => p.Input, input)
+        );
+        
+        cut.FindAll("button")[0].Click();
+        // string markup = "";
+        // cut.FindAll("input")[1].MarkupMatches(markup);
+    
+        JWTDebugger jwtDebugger = cut.Instance;
+        Assert.Equal(jwtDebugger.Input, input);
+        Assert.Equal(jwtDebugger.Header, expectedHeader);
+        Assert.Equal(jwtDebugger.Payload, expectedPayload);
+        Assert.Equal(jwtDebugger.Signature, exptectedSignature);
+    }
 
 }
