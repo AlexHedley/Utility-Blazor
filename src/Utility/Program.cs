e32a0e31d54c5abe232fb6ebe3cae4fc5e6da528
Alex Hedley <alex.hedley@opencastsoftware.com>
2024-03-07 07:05:59 +0000

    Site Settings (Theme/Colour)

--- src/Utility/Program.cs
+++ src/Utility/Program.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 using Blazored.LocalStorage;
 
 using Utility;
+using Utility.Infrastructure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddFluentUIComponents();
+builder.Services.AddScoped<CacheStorageAccessor>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 
