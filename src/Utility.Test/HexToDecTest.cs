12aa1e54ab4398b939ffc1dda2e6a4d407466cc9
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-11-18 16:05:17 +0000

    WIP missing test templates

---
+++ src/Utility.Test/HexToDecTest.cs
@@ -0,0 +1,11 @@
+using Bunit;
+using Xunit;
+
+namespace Utility.Test;
+
+public class HexToDecTest : TestContext
+{
+    public HexToDecTest() {}
+    
+    // [Fact]
+}
---
+++ src/Utility.Test/SQLInputTest.cs
@@ -0,0 +1,11 @@
+using Bunit;
+using Xunit;
+
+namespace Utility.Test;
+
+public class SQLInputTest : TestContext
+{
+    public SQLInputTest() {}
+    
+    // [Fact]
+}
